a2617b39505eb68e4c62b789249eabba72217b3f
Aleksander Morgado <aleksander@aleksander.es>
2017-07-29 09:55:07 +0200

    aosp: setup build for AOSP
    E.g. in you local_manifests:
    
      <remote name="aleksander" fetch="https://github.com/aleksander0m/" />
      <remove-project path="external/tinyalsa" name="platform/external/tinyalsa" />
      <project path="external/tinyalsa" name="android-external-tinyalsa.git" revision="aosp" remote="aleksander" groups="default" />

--- Android.mk
+++ Android.mk
@@ -1,12 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-srcdir ?= $(LOCAL_PATH)/../src
-incdir ?= $(LOCAL_PATH)/../include
-utilsdir ?= $(LOCAL_PATH)/../utils
-
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= $(incdir)
-LOCAL_SRC_FILES:= $(srcdir)/mixer.c $(srcdir)/pcm.c
+LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= src/mixer.c src/pcm.c
 LOCAL_MODULE := libtinyalsa
 LOCAL_SHARED_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
@@ -14,32 +10,32 @@ LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= $(incdir)
-LOCAL_SRC_FILES:= $(utilsdir)/tinyplay.c
+LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= utils/tinyplay.c
 LOCAL_MODULE := tinyplay
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= $(incdir)
-LOCAL_SRC_FILES:= $(utilsdir)/tinycap.c
+LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= utils/tinycap.c
 LOCAL_MODULE := tinycap
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= $(incdir)
-LOCAL_SRC_FILES:= $(utilsdir)/tinymix.c
+LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= utils/tinymix.c
 LOCAL_MODULE := tinymix
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= $(incdir)
-LOCAL_SRC_FILES:= $(utilsdir)/tinypcminfo.c
+LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= utils/tinypcminfo.c
 LOCAL_MODULE := tinypcminfo
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
